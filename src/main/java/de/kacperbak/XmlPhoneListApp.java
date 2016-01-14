d9c5246446a8cf6a142ef1fc0f19c693e16b0bb6
Kacper Bak <github@kacperbak.de>
2016-01-14 21:20:38 +0100

    Renaming fixed

--- src/main/java/de/kacperbak/XmlPhoneListApp.java
+++ src/main/java/de/kacperbak/XmlPhoneListApp.java
@@ -2,14 +2,10 @@ package de.kacperbak;
 
 import de.kacperbak.domain.PhoneItems;
 
-import java.io.File;
-
 /**
  * Created by bakka on 14.01.16.
  */
 public class XmlPhoneListApp {
-
-
 
     public static void main(String[] args) {
         CliUtils.welcome();
