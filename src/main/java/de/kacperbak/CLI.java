ad801010753c1ffeb3a3f64602809bde3d65bfe4
Kacper Bak <github@kacperbak.de>
2016-01-14 19:27:35 +0100

    Basic project structure created

---
+++ src/main/java/de/kacperbak/CLI.java
@@ -0,0 +1,13 @@
+package de.kacperbak;
+
+/**
+ * Created by bakka on 14.01.16.
+ */
+public class Cli {
+
+    public static final String WELCOME = "Welcome to Xml phone list 2000!";
+
+    public static void welcome(){
+        System.out.println(WELCOME);
+    }
+}
---
+++ src/main/java/de/kacperbak/XmlPhoneListApp.java
@@ -0,0 +1,11 @@
+package de.kacperbak;
+
+/**
+ * Created by bakka on 14.01.16.
+ */
+public class XmlPhoneListApp {
+
+    public static void main(String[] args) {
+        Cli.welcome();
+    }
+}
